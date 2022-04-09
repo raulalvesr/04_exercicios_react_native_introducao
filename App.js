ec7a7fc7f5349abfd4f6ab6aac54642070ca6852
leonardo <leo@gmail.com>
2022-04-08 21:49:28 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [numeros, setNumeros] = useState([]);
+
+  // setNumeros = () => {
+  //   while(true) {
+  //     if (numeros.lengt >= 6) {
+  //       break;
+  //     }
+      
+  //     const generatedNumber = Math.random() * (60 - 1) + 1;
+  //     if (!numeros.includes(generatedNumber)) {
+  //       continue;
+  //     }
+
+  //     numeros.push(generatedNumber);
+  //   }
+  // }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{numeros}</Text>
+      {/* <Button title="Sortear" onPress={() => setNumeros()}/> */}
       <StatusBar style="auto" />
     </View>
   );
